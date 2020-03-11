33b6593232cc17020da30d5c1f7cc30462390ace
Shrutika-Ingale <61141596+Shrutika-Ingale@users.noreply.github.com>
2020-03-11 18:38:10 +0530

    Removing TokenHelper class as there is no authentication implemented in the project

--- Kmd.Momentum.Mea.Api.Tests/HealthCheckTests.cs
+++ Kmd.Momentum.Mea.Api.Tests/HealthCheckTests.cs
@@ -2,11 +2,13 @@
 using System.Net;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
-using Kmd.Momentum.Mea.Test.Common;
+using Kmd.Momentum.Mea.Api;
+using Kmd.Momentum.Mea.Integration.Tests;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
 using Xunit;
 
-namespace Kmd.Momentum.Mea.Api.Tests
+namespace Kmd.Momentum.Mea.Integration.Tests
 {
     public class HealthCheckTests : IClassFixture<WebApplicationFactory<Startup>>
     {
@@ -22,15 +24,12 @@ namespace Kmd.Momentum.Mea.Api.Tests
         {
             //Arrange
             var client = _factory.CreateClient();
-            var tokenHelper = new TokenHelper();
-            var accessToken = await tokenHelper.GetToken().ConfigureAwait(false);
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
 
             //Act
             var response = await client.GetAsync(new Uri("Health/Ready", UriKind.Relative)).ConfigureAwait(false);
 
             //Assert
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);           
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         }
     }
 }

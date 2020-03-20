1794bd16b2e91d96f1b5194bb83c97fb30d922fa
Shrutika-Ingale <61141596+Shrutika-Ingale@users.noreply.github.com>
2020-03-20 18:10:17 +0530

    Adding intergration tests for citizen apis

---
+++ test/Kmd.Momentum.Mea.Integration.Tests/Citizen/CitizenTests.cs
@@ -0,0 +1,78 @@
+﻿using Kmd.Momentum.Mea.Api;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+using Kmd.Momentum.Mea.Citizen.Model;
+
+namespace Kmd.Momentum.Mea.Integration.Tests.Citizen
+{
+    public class CitizenTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public CitizenTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task GetActiveCitizens()
+        {
+            //Arrange
+            var client = _factory.CreateClient();
+            var requestUri = "/citizens";
+
+            //Act
+            var response = await client.GetAsync(requestUri).ConfigureAwait(false);
+            var result = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            var actualResponse = JsonConvert.DeserializeObject<string[]>(result);
+
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            actualResponse.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task GetCitizenByCpr()
+        {
+            //Arrange
+            var client = _factory.CreateClient();
+            var cprNumber = "2811733914";
+            var requestUri = $"/citizens/cpr/{cprNumber}";
+
+            //Act
+            var response = await client.GetAsync(requestUri).ConfigureAwait(false);
+            var responseBody = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            var actualResponse = JsonConvert.DeserializeObject<CitizenDataResponseModel>(responseBody);
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            actualResponse.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task GetCitizenByCitizenId()
+        {
+            //Arrange
+            var client = _factory.CreateClient();
+            var citizenId = "70375a2b-14d2-4774-a9a2-ab123ebd2ff6";
+            var requestUri = $"/citizens/kss/{citizenId}";
+
+            //Act
+            var response = await client.GetAsync(requestUri).ConfigureAwait(false);
+            var responseBody = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            var actualResponse = JsonConvert.DeserializeObject<CitizenDataResponseModel>(responseBody);
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            actualResponse.Should().NotBeNull();
+        }
+    }
+}

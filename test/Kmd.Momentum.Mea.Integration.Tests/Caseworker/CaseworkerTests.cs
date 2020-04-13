92d22aeaecb580c7256d89561d94b604f7af16a4
Dommaraju Umamaheswari <DU00556591@INBASDLP06496>
2020-04-13 15:59:30 +0530

    changes done and added integration test case

---
+++ test/Kmd.Momentum.Mea.Integration.Tests/Caseworker/CaseworkerTests.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using Kmd.Momentum.Mea.Caseworker.Model;
+using Kmd.Momentum.Mea.Caseworker1.Model;
+using Moq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Kmd.Momentum.Mea.Integration.Tests.Caseworker
+{
+    public class CaseworkerTests : IClassFixture<IntegrationTestApplicationFactory>
+    {
+        private readonly IntegrationTestApplicationFactory _factory;
+
+        public CaseworkerTests(IntegrationTestApplicationFactory factory)
+        {
+            _factory = factory;
+        }
+
+        [SkipLocalFact]
+        public async Task GetAllCaseworkersSuccess()
+        {
+            //Arrange       
+            var clientMoq = _factory.CreateClient();
+
+            var tokenHelper = new TokenGenerator();
+            var accessToken = await tokenHelper.GetToken().ConfigureAwait(false);
+
+            clientMoq.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
+
+            //Act
+            var response = await clientMoq.GetAsync($"/caseworkers").ConfigureAwait(false);
+            var result = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            var actualResponse = JsonConvert.DeserializeObject<List<CaseworkerDataResponseModel>>(result);
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            actualResponse.Should().NotBeNullOrEmpty();
+            actualResponse.Count.Should().BeGreaterThan(0);
+        }
+    }
+}

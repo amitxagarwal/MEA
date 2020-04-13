7bd3e3f1f278c1896c568b408cd08702faf9cf2f
Ashish Verma <QZG@kmd.dk>
2020-04-13 13:27:24 +0530

    Added clientID in the logs

--- src/Kmd.Momentum.Mea.Common/Authorization/MeaCustomClaimsCheck.cs
+++ src/Kmd.Momentum.Mea.Common/Authorization/MeaCustomClaimsCheck.cs
@@ -8,19 +8,23 @@ namespace Kmd.Momentum.Mea.Common.Authorization
     {
         public MeaTokenClaimResponse FetchClaims(AuthorizationHandlerContext context, string audience, string tenant, string scope)
         {
+            var clientId = context.User.Claims.FirstOrDefault(x => x.Type == "azp").Value;
+
             // If user does not have the audience, tenant and scope claim, get out of here
             if ((!context.User.HasClaim(c => c.Type == audience)) &&
                 (!context.User.HasClaim(c => c.Type == tenant)) &&
                 (!context.User.HasClaim(c => c.Type == scope)) is true)
             {
-                Log.Error("The token to access the MEA does not contains all the relevant claims");
+                Log.ForContext("ClientId", clientId)
+                    .Error("The token to access the MEA does not contains all the relevant claims");
 
                 return null;
             }
 
-            Log.Information("The token to access the MEA contains all the relevant claims");
+            Log.ForContext("ClientId", clientId)
+                .Information("The token to access the MEA contains all the relevant claims");
 
-            var tokenClaims = GetTokenClaims(context);
+            var tokenClaims = GetTokenClaims(context, clientId);
 
             if (tokenClaims != null)
             {
@@ -30,7 +34,7 @@ namespace Kmd.Momentum.Mea.Common.Authorization
             return null;
         }
 
-        private MeaTokenClaimResponse GetTokenClaims(AuthorizationHandlerContext context)
+        private MeaTokenClaimResponse GetTokenClaims(AuthorizationHandlerContext context, string clientId)
         {
             // Split the audience, tenants, scope string into an array
             var audience = context.User.FindFirst(c => c.Type == MeaCustomClaimAttributes.AudienceClaimTypeName).Value.Split(' ');
@@ -39,11 +43,13 @@ namespace Kmd.Momentum.Mea.Common.Authorization
 
             if (audience.Length == 0 || tenant == null || scope.Length == 0)
             {
-                Log.Error("Could not fetch the value of the MEA token claims");
+                Log.ForContext("ClientId", clientId)
+                    .Error("Could not fetch the value of the MEA token claims");
                 return null;
             }
 
             Log.ForContext("Tenant", tenant)
+                .ForContext("ClientId", clientId)
                 .Information("The audience, tenant and scope values from the MEA token are fetched successfully");
 
             return new MeaTokenClaimResponse(audience, tenant, scope);

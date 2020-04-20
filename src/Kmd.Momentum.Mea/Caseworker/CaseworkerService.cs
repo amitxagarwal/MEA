adc3c72dc96c4ebc54cfc80720804b81efeb01c5
Ashish Verma <QZG@kmd.dk>
2020-04-20 15:13:37 +0530

    commit

--- src/Kmd.Momentum.Mea/Caseworker/CaseworkerService.cs
+++ src/Kmd.Momentum.Mea/Caseworker/CaseworkerService.cs
@@ -69,7 +69,7 @@ namespace Kmd.Momentum.Mea.Caseworker
             var citizenDataObj = JsonConvert.DeserializeObject<CaseworkerDataResponse>(content);
 
             var caseworkerData = new CaseworkerDataResponseModel(citizenDataObj.Id, citizenDataObj.DisplayName, citizenDataObj.GivenName,
-                citizenDataObj.MiddleName, citizenDataObj.Initials, citizenDataObj.Email !=null ? citizenDataObj.Email.Address :null, citizenDataObj.Phone != null ? citizenDataObj.Phone.Number :null, citizenDataObj.CaseworkerIdentifier,
+                citizenDataObj.MiddleName, citizenDataObj.Initials, citizenDataObj.Email?.Address, citizenDataObj.Phone?.Number, citizenDataObj.CaseworkerIdentifier,
                 citizenDataObj.Description, citizenDataObj.IsActive, citizenDataObj.IsBookable);
         
             Log.ForContext("CorrelationId", _correlationId)

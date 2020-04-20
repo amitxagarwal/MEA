9a26cc6c694ba627e4d652150a1407054f53182b
Dommaraju Umamaheswari <DU00556591@INBASDLP06496>
2020-04-20 14:38:12 +0530

    commit

--- src/Kmd.Momentum.Mea/Caseworker/CaseworkerService.cs
+++ src/Kmd.Momentum.Mea/Caseworker/CaseworkerService.cs
@@ -68,7 +68,7 @@ namespace Kmd.Momentum.Mea.Caseworker
             var content = response.Result;
             var citizenDataObj = JsonConvert.DeserializeObject<CaseworkerDataResponse>(content);
             var caseworkerData = new CaseworkerDataResponseModel(citizenDataObj.Id, citizenDataObj.DisplayName, citizenDataObj.GivenName,
-                citizenDataObj.MiddleName, citizenDataObj.Initials, citizenDataObj.Email.Address, citizenDataObj.Phone.Number, citizenDataObj.CaseworkerIdentifier,
+                citizenDataObj.MiddleName, citizenDataObj.Initials, citizenDataObj.Email !=null ? citizenDataObj.Email.Address :null, citizenDataObj.Phone != null ? citizenDataObj.Phone.Number :null, citizenDataObj.CaseworkerIdentifier,
                 citizenDataObj.Description, citizenDataObj.IsActive, citizenDataObj.IsBookable);
         
             Log.ForContext("CorrelationId", _correlationId)

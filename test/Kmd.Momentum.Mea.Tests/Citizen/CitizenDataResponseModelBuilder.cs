35d38ff478ef45b8a8f092c5c9e123845f28edb1
shrut <shrut@DESKTOP-TMASBS9>
2020-04-30 19:55:55 +0530

    Commit3

--- test/Kmd.Momentum.Mea.Tests/Citizen/CitizenDataResponseModelBuilder.cs
+++ test/Kmd.Momentum.Mea.Tests/Citizen/CitizenDataResponseModelBuilder.cs
@@ -2,36 +2,79 @@
 
 namespace Kmd.Momentum.Mea.Tests.Citizen
 {
-    public class CitizenDataResponseBuilder
+    public class CitizenDataResponseModelBuilder
     {
-        private readonly string citizenId = "testCpr";
-        private readonly string displayName = "testBody";
-        private readonly string givenName = "testTitle";
-        private readonly string middleName = "testType";
-        private readonly string initials = "testType";
-        private readonly string email = "testType";
-        private readonly string phone = "testType";
-        private readonly string caseworkerIdentifier = "testType";
-        private readonly string description = "description";
-        private readonly bool isBookable = true;
-        private readonly bool isActive = true;
+        private string citizenId = "testCpr";
+        private string displayName = "testBody";
+        private string givenName = "testTitle";
+        private string middleName = "testType";
+        private string initials = "testType";
+        private string email = "testType";
+        private string phone = "testType";
+        private string caseworkerIdentifier = "testType";
+        private string description = "description";
+        private bool isBookable = true;
+        private bool isActive = true;
 
         public CitizenDataResponseModel Build()
         {
-            return new CitizenDataResponseModel(citizenId, displayName, givenName, middleName, initials, email, phone, caseworkerIdentifier, description, true, true)
-            {
-                CitizenId = citizenId,
-                DisplayName = displayName,
-                GivenName = givenName,
-                MiddleName = middleName,
-                Initials = initials,
-                Email = email,
-                Phone = phone,
-                CaseworkerIdentifier = caseworkerIdentifier,
-                Description = description,
-                IsBookable = isBookable,
-                IsActive = isActive
-            };
+            return new CitizenDataResponseModel(citizenId, displayName, givenName, middleName, initials, email, phone, caseworkerIdentifier, description, true, true);
+        }
+
+        public CitizenDataResponseModelBuilder WithCpr(string citizenId)
+        {
+            this.citizenId = citizenId;
+            return this;
+        }
+        public CitizenDataResponseModelBuilder WithDisplayName(string displayName)
+        {
+            this.displayName = displayName;
+            return this;
+        }
+        public CitizenDataResponseModelBuilder WithGivenName(string givenName)
+        {
+            this.givenName = givenName;
+            return this;
+        }
+        public CitizenDataResponseModelBuilder WithMiddleName(string middleName)
+        {
+            this.middleName = middleName;
+            return this;
+        }
+        public CitizenDataResponseModelBuilder Withinitials(string initials)
+        {
+            this.initials = initials;
+            return this;
+        }
+        public CitizenDataResponseModelBuilder WithEmail(string email)
+        {
+            this.email = email;
+            return this;
+        }
+        public CitizenDataResponseModelBuilder WithPhone(string phone)
+        {
+            this.phone = phone;
+            return this;
+        }
+        public CitizenDataResponseModelBuilder WithCaseworkerIdentifier(string caseworkerIdentifier)
+        {
+            this.caseworkerIdentifier = caseworkerIdentifier;
+            return this;
+        }
+        public CitizenDataResponseModelBuilder WithDescription(string description)
+        {
+            this.description = description;
+            return this;
+        }
+        public CitizenDataResponseModelBuilder WithIsBookable(bool isBookable)
+        {
+            this.isBookable = isBookable;
+            return this;
+        }
+        public CitizenDataResponseModelBuilder WithIsActive(bool isActive)
+        {
+            this.isActive = isActive;
+            return this;
         }
     }
 }

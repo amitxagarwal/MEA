f52f22db95395212d8c898663a3393041153a800
Dommaraju Umamaheswari <DU00556591@INBASDLP06496>
2020-05-05 19:55:16 +0530

    changes done

--- src/Kmd.Momentum.Mea/TaskApi/Model/AssignedActors.cs
+++ src/Kmd.Momentum.Mea/TaskApi/Model/AssignedActors.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Kmd.Momentum.Mea.TaskApi.Model
 {
     public class AssignedActors
     {
-        public string Id { get; set; }
+        public Guid Id { get; set; }
         public string DisplayName { get; set; }
     }
 }

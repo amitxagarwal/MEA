4c04750c56357e19641acbe8a374c42a70f348f3
Ashish Verma <QZG@kmd.dk>
2020-05-07 12:08:21 +0530

    commit

--- test/Kmd.Momentum.Mea.Integration.Tests/Caseworker/CaseworkerTests.cs
+++ test/Kmd.Momentum.Mea.Integration.Tests/Caseworker/CaseworkerTests.cs
@@ -122,7 +122,7 @@ namespace Kmd.Momentum.Mea.Integration.Tests.Caseworker
             var tokenHelper = new TokenGenerator();
             var accessToken = await tokenHelper.GetToken().ConfigureAwait(false);
 
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6InpGTDB5ZGF2NDgxSHJDT2M0bk9MdTR2cDB2SDZxeTBvUS1SdlBzQW1vNlkifQ.eyJpc3MiOiJodHRwczovL2xvZ2ljaWRlbnRpdHlwcm9kLmIyY2xvZ2luLmNvbS9hYTBkNmYzYi04ZmIyLTQ3N2QtYWI4OC1iYjJlNjY0NjIwZTgvdjIuMC8iLCJleHAiOjE1ODg4MzU4NTksIm5iZiI6MTU4ODgzMjI1OSwiYXVkIjoiNjlkOTY5M2UtYzRiNy00Mjk0LWEyOWYtY2RkYWViZmE1MThiIiwic3ViIjoiMWQxOGQxNTEtNTE5Mi00N2YxLWE2MTEtZWZhNTBkYmRjNDMxIiwiaWRwIjoiTG9naWNJZGVudGl0eSIsImF1dGhlbnRpY2F0aW9uU291cmNlIjoiQ2xpZW50IENyZWRlbnRpYWxzIiwidGVuYW50IjoiMTU5Iiwibm9uY2UiOiJkZWZhdWx0Tm9uY2UiLCJzY3AiOiJhY2Nlc3MiLCJhenAiOiIxZDE4ZDE1MS01MTkyLTQ3ZjEtYTYxMS1lZmE1MGRiZGM0MzEiLCJ2ZXIiOiIxLjAiLCJpYXQiOjE1ODg4MzIyNTl9.LfxDczrKP15Lh8oLc7pnJneyJnxq2pgqxbqIqZagOiHFkkaFcslcp4gSHUDPIjHlINTvsWG8uYkIMubDkpSD8qmVXX7T162pGHzjhixeiqE3cQVbpaiDdPwD7WfC4H8nTwgQFiJ6NYIQJoARQObduTzNkkiURaViDy4-cgPp-efQjEiOgDRRoPBzEvy8j1WGknAtrdW0QjD137TRH77uh7FeOBPK_R6RXS_Ln_ZoeSqZ7NQC0MsINTDBi1WrTc-YlKXQ6Oz3FEJFb2CGkUaBuWhpQTF9g7MdLZL7SFwrRLxiKIfy1RGN6lW-0rZA5lP65j_SiEd_c47hiy7v2HoQHw");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
 
             //Act
             var response = await client.GetAsync(requestUri).ConfigureAwait(false);
